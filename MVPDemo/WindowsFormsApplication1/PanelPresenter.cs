b70702f55248d004b72fde0ca2caa5f7d790d05e
Boston Hsu <bostonhsu@outlook.com>
2017-06-29 11:48:44 +0800

    Presenter
    在Presenter中，创建View属性。在构造函数中建立Model、View和Presenter的关系，并初始化Model（也就是）的值：

---
+++ MVPDemo/WindowsFormsApplication1/PanelPresenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class PanelPresenter
+    {
+        public PanelPresenter(Form1 view)
+        {
+            this.View = view;
+
+            //初始化Model
+            this.View.Model = new PanelPresenterationModel() { Name = "Bao, Jianqiang" };
+        }
+
+        public Form1 View { get; set; }
+    }
+}
