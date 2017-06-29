8c94eff8ec4c5fc2f6423b66b02429c33128ce60
Boston Hsu <bostonhsu@outlook.com>
2017-06-29 16:20:36 +0800

    新建一个类库，里面定义接口，这里定义两个方法，一个有返回值的，一个无返回值的。

--- MVPDemo/PluginDemo/IMsgPlug.cs
+++ MVPDemo/PluginDemo/IMsgPlug.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace PluginDemo
 {
+    /// <summary>
+    /// 这是插件必须实现的接口，也是主程序与插件通信的唯一接口
+    /// 换句话说，主程序只认识插件里的这些方法
+    /// </summary>
     public interface IMsgPlug
     {
         void OnShowDlg();
