d915a98a964139335f8553d93d40268539848c91
Boston Hsu <bostonhsu@outlook.com>
2017-06-29 10:27:08 +0800

    构建了View的下一级接口。

---
+++ MVPDemo/MVPDemo/IMainForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MVPDemo
+{
+    public interface IMainForm<T> : IView<T>
+    {
+        Button TestButton { get; }
+        TextBox TestTextBox { get; }
+        event EventHandler ViewLoadEvent;
+        event EventHandler ButtonSubmitEvent;
+        void ShowSubmitDialog();
+    }
+}
