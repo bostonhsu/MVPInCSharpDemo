487867bcfa5d8d8727b2984f8d0d4103689034d2
Boston Hsu <bostonhsu@outlook.com>
2017-06-29 10:36:53 +0800

    创建View的实现类。

---
+++ MVPDemo/MVPDemo/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVPDemo
+{
+    public partial class MainForm : MvpForm, IMainForm<MainFormModel>
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public MainFormModel Model { get; set; }
+        public TextBox TestTextBox { get { return txtText; } }
+        public Button TestButton { get { return btnSubmit; } }
+
+        public event EventHandler ViewLoadEvent;
+        public event EventHandler ButtonSubmitEvent;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (ViewLoadEvent != null)
+            {
+                ViewLoadEvent(sender, e);
+            }
+        }
+
+        public void ShowSubmitDialog()
+        {
+            MessageBox.Show("to submit?");
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ButtonSubmitEvent != null)
+            {
+                ButtonSubmitEvent(sender, e);
+            }
+        }
+    }
+}
