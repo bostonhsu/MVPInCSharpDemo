39d465ca57ca7d3458c40e2b288ee0e6087e49a6
Boston Hsu <bostonhsu@outlook.com>
2017-06-29 16:44:35 +0800

    将上面的类库生成IMsg.dll,新建一个类库PluginOne
    添加刚出的引用，分别新建两个类来实现IMsg中定义的接口。

---
+++ MVPDemo/PluginOne/myConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IMsg;
+
+namespace PluginOne
+{
+    public class myConsole : IMsgPlug
+    {
+        public void OnShowDlg()
+        {
+            Console.WriteLine("控制台调用插件的OnShowDlg方法");
+        }
+
+        public string OnShowInfo()
+        {
+            return "myConsole";
+        }
+    }
+}
